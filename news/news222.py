942d7f09d7637a8a3a67fe290837ab02b174a8af
wau2380(yijung park) <79182893+wau2380@users.noreply.github.com>
2022-09-05 15:48:44 +0900

    Update news222.py

--- news/news222.py
+++ news/news222.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-
 from webdriver_manager.Chrome import ChromeDriverManager
